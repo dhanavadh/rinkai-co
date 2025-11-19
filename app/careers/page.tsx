f20e8269553ac54753e0072c74f043d93aecc2af
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-19 15:44:22 +0700

    fix: banner struct

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -3,15 +3,14 @@ import Banner from "../components/common/Banner";
 export default function CareersPage() {
   return (
     <>
-      <section>
-        <Banner
-          title="Careers at Rinkai"
-          description="Rinkai. Make life easier"
-          imageSrc="/images/IMG_1320.webp"
-          href="/"
-          buttonText="ดูตำแหน่งงาน"
-        />
-      </section>
+      <Banner
+        title="Careers at Rinkai"
+        description="Rinkai. Make life easier"
+        imageSrc="/images/IMG_1320.webp"
+        href="/"
+        buttonText="ดูตำแหน่งงาน"
+      />
+      <section className="flex items-center justify-center w-full min-h-screen"></section>
     </>
   );
 }
--- app/components/common/Banner.tsx
+++ app/components/common/Banner.tsx
@@ -9,17 +9,21 @@ interface BannerProps {
   buttonText: string;
 }
 
-export default function Banner({ title, description, imageSrc, href, buttonText }: BannerProps) {
+export default function Banner({
+  title,
+  description,
+  imageSrc,
+  href,
+  buttonText,
+}: BannerProps) {
   return (
     <>
-      <div className="relative flex h-[500px]">
+      <div className="relative flex h-[500px] w-full font-sans">
         <div className="flex items-center max-w-7xl w-full mx-auto">
           <section className="z-10 flex flex-col gap-4 mt-16 p-6">
             <span>
               <p className="text-5xl text-white mb-2">{title}</p>
-              <p className="text-lg text-white">
-                {description}
-              </p>
+              <p className="text-lg text-white">{description}</p>
             </span>
             <Link
               href={href}
